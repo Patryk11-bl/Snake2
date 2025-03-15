1969b55e62946227831a743244a1e969c50dc30d
Patryk11-bl <bloch.patryk.pb1@gmail.com>
2025-03-15 14:41:40 +0100

    1.1

---
+++ Projekt.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+import time
+
+
+SZEROKOSC_EKRANU = 800
+WYSOKOSC_EKRANU = 608
+
+tlo = pygame.Surface((SZEROKOSC_EKRANU, WYSOKOSC_EKRANU))
+
+for i in range(25):
+    for j in range(19):
+        
